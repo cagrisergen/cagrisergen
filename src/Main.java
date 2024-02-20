bcb9bfc67dff99d6e0e28f291395f2ac3e755be9
cagrisergen <cagrisergen@hotmail.com>
2024-02-20 16:42:33 +0300

    sdsdd

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner (System.in);
+        System.out.println("bir sayı giriniz: ");
+        int number = input.nextInt();
+        boolean isPrime = true;
+        for (int i = 2; i < number; i++){
+            if (number % i == 0){
+                isPrime = false;
+                break;
+                
+                
+                
+                
+            }
+        }
+        
+        if (isPrime) {
+            System.out.println(number + " sayısı ASALDIR ");
+            
+            
+        }else{
+            System.out.println(number  + "sayısı ASAL değildir");
+            
+        }
+    }
+        
+        
+    
+}
